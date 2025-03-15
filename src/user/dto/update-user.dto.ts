898c91cde112ca6c369d73fdf6832b3bdbdfecc9
HungNguyen <21520252@gm.uit.edu.vn>
2025-03-15 23:02:05 +0700

    refactor: clean up import statements in update-user.dto.ts

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,7 +1,4 @@
-import {
-  PartialType,
-  PickType,
-} from '@nestjs/swagger';
+import { PartialType, PickType } from '@nestjs/swagger';
 import { User } from 'src/entities';
 
 export class UpdateUserDto extends PartialType(
