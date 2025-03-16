ea92a3a40cab72a86cef6b60dcafac172c89fbe8
HungNguyen <21520252@gm.uit.edu.vn>
2025-03-16 16:42:31 +0700

    refactor: streamline import statements in user-friend.controller.ts

--- src/user-friend/user-friend.controller.ts
+++ src/user-friend/user-friend.controller.ts
@@ -1,8 +1,4 @@
-import {
-  Controller,
-  Get,
-  Query,
-} from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { UserFriendService } from './user-friend.service';
 import { User } from 'src/common/decorators';
 import { FilterFriendsDto } from 'src/user-friend/dto/filter-posts.dto';
