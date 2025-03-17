3b497996b518ca92dbb9d564ba60832784af37ab
HungNguyen <21520252@gm.uit.edu.vn>
2025-03-17 21:16:36 +0700

    fix: format ApiPropertyOptional description for clarity in FilterFriendsDto

--- src/user-friend/dto/filter-friend.dto.ts
+++ src/user-friend/dto/filter-friend.dto.ts
@@ -11,6 +11,8 @@ export class FilterFriendsDto extends GenericFilter {
   @ApiPropertyOptional({ description: 'Include mutual friends count' })
   withMutualFriendsCount?: boolean;
 
-  @ApiPropertyOptional({ description: 'Include pending request to current user' })
+  @ApiPropertyOptional({
+    description: 'Include pending request to current user',
+  })
   withCurrentUserPendingRequest?: boolean;
 }
