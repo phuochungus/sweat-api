e00e46fb21627b979399a3b950fe83cfd2a88adc
HungNguyen <21520252@gm.uit.edu.vn>
2025-05-01 23:50:55 +0700

    save

--- src/entities/post.entity.ts
+++ src/entities/post.entity.ts
@@ -12,12 +12,15 @@ export class Post extends BaseEntity {
   text!: string;
 
   @Column({ default: 0 })
+  @ApiProperty()
   commentCount!: number;
 
   @Column({ default: 0 })
+  @ApiProperty()
   reactCount!: number;
 
   @Column({ default: 0 })
+  @ApiProperty()
   mediaCount!: number;
 
   @Column({ type: 'enum', enum: PostPrivacy })
@@ -29,12 +32,14 @@ export class Post extends BaseEntity {
   userId!: number;
 
   @Column({ nullable: true })
+  @ApiProperty()
   location?: string;
 
   @ManyToOne(() => User)
   @JoinColumn({ name: 'user_id' })
   user: User;
 
+  @ApiProperty()
   @OneToMany(() => PostMedia, (postMedia) => postMedia.post, { cascade: true })
   postMedia: PostMedia[];
 }
