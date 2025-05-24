39bea00c46c6b2621b67179056d2f6363ca0fd0d
HungNguyen <21520252@gm.uit.edu.vn>
2025-05-24 23:16:09 +0700

    style(migrations, seeds): standardize quotes and simplify migration structure

--- migrations/1748103056082-init.ts
+++ migrations/1748103056082-init.ts
@@ -1,11 +1,7 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Init1748103056082 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {}
 }
