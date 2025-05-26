371799d2c42a2af130e01548005740d6f74591f7
HungNguyen <21520252@gm.uit.edu.vn>
2025-05-26 15:24:29 +0700

    save

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -113,7 +113,7 @@ export class PostService {
     if (createdBy) {
       queryBuilder.andWhere('post.userId = :userId', { userId: createdBy });
     } else {
-      const friendIds = [];
+      const friendIds = [Number(createdBy)];
       if (currentUserId) {
         // Get user's friends
         const friends = await this.friendRepository.find({
