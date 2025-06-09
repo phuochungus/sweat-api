7c3ad533590e89b7bef2c49f9598c1a84ac767d5
HungNguyen <21520252@gm.uit.edu.vn>
2025-06-09 08:47:17 +0700

    feat(user-friend): add unique index to userId1 and userId2 columns

--- src/entities/user-friend.entity.ts
+++ src/entities/user-friend.entity.ts
@@ -1,7 +1,8 @@
-import { Entity, Column } from 'typeorm';
+import { Entity, Column, Index } from 'typeorm';
 import { BaseEntity } from './base.entity';
 
 @Entity('user_friend')
+@Index(['userId1', 'userId2'], { unique: true })
 export class UserFriend extends BaseEntity {
   @Column()
   userId1!: number;
